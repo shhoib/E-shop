a5fc117da7a65e5c0f29612674cd56234a035c83
Shuaib Salam <shuaibsalam1234@gmail.com>
2024-03-26 16:47:48 +0530

    final commit

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -3,7 +3,7 @@ import { NextResponse, NextRequest } from "next/server";
 import Stripe from "stripe";
 
  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY! as string, {
-    apiVersion: "2022-11-15",
+    apiVersion: "2023-10-16",
    });
 
 
